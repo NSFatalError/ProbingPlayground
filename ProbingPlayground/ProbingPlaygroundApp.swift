6081d362120c9bd847e10b41937b5876f14ea7c7
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2025-05-07 18:22:26 +0200

    Initial Commit

---
+++ ProbingPlayground/ProbingPlaygroundApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProbingPlaygroundApp.swift
+//  ProbingPlayground
+//
+//  Created by Kamil Strzelecki on 07/05/2025.
+//  Copyright © 2025 Kamil Strzelecki. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ProbingPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProbingPlaygroundTests/ProbingPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProbingPlaygroundTests.swift
+//  ProbingPlayground
+//
+//  Created by Kamil Strzelecki on 07/05/2025.
+//  Copyright © 2025 Kamil Strzelecki. All rights reserved.
+//
+
+import Testing
+
+struct ProbingPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
