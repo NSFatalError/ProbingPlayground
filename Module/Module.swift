a4374d70f4823afa94bf1c2db1f48a3b29302761
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2025-05-08 17:32:17 +0200

    -

---
+++ Module/Module.swift
@@ -0,0 +1,9 @@
+//
+//  Module.swift
+//  ProbingPlayground
+//
+//  Created by Kamil Strzelecki on 08/05/2025.
+//  Copyright © 2025 Kamil Strzelecki. All rights reserved.
+//
+
+internal import Probing
---
+++ ModuleTests/ModuleTests.swift
@@ -0,0 +1,11 @@
+//
+//  ModuleTests.swift
+//  ProbingPlayground
+//
+//  Created by Kamil Strzelecki on 08/05/2025.
+//  Copyright © 2025 Kamil Strzelecki. All rights reserved.
+//
+
+@testable import Module
+import ProbeTesting
+import Testing
